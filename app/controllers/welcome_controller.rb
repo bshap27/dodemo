a61a749c62b8c02b238d6ef45e4998c8fd0ee3a1
bshap27 <rshapiro3@gmail.com>
2017-11-22 11:33:04 -0500

    counter changed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,6 @@ class WelcomeController < ApplicationController
     counter = Counter.find_or_create_by(name: "default")
     counter.value = (counter.value || 0) + 1
     counter.save!
-    render plain: "Counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
+    render plain: "Awesome counter #{counter.value}; #{Rails.version}/#{RUBY_VERSION}"
   end
 end
